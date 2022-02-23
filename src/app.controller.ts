7cf2bf018da598b42954b06260f778cce68ca543
timi-o3 <86927745+timi-o3@users.noreply.github.com>
2022-02-23 01:08:27 +0100

    home page update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('home.html')
+  getHome(): {} {
+    return this.appService.getHome();
+  }
+
+  @Get('about-us')
+  @Render('about-us.html')
+  getAboutUs(): {} {
+    return this.appService.getAboutUs();
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('hello2')
+  @Render('index.html')
+  getHello2():{}{
+    return this.appService.getHello2();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  getHome(): {} {
+    return {title: 'Home Page'};
+  }
+
+  getAboutUs(): {} {
+    return{title: 'About Us Page'}
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getHello2(): {} {
+    return {message: 'Hello World', title:'My Greeting App'};
   }
 }
